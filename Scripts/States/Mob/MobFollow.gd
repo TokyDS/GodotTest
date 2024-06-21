c740672b5d8d055c7235fb3858ccd9e9493675a8
TokyDS <97611336+TokyDS@users.noreply.github.com>
2024-06-22 04:46:47 +0500

    Updated Navigation in Follow State and Wandering State(IdleState)

--- Scripts/States/Mob/MobFollow.gd
+++ Scripts/States/Mob/MobFollow.gd
@@ -6,21 +6,40 @@ class_name MobFollow
 var player : CharacterBody2D
 @onready var navigation_agent_2d = $"../../NavigationAgent2D"
 
-func enter(): 
+func enter():
 	player = get_tree().get_first_node_in_group("Player")
-#func seeker_setup():
-	#await get_tree().physics_frame
-	#if target:
-		#navigation_agent_2d.target_position = target.global_position
+	call_deferred("seeker_setup")
+	
+func seeker_setup():
+	await get_tree().physics_frame
+	if player:
+		navigation_agent_2d.target_position = player.global_position
 
 func physics_update(delta : float):
-	var direction = player.global_position - mob.global_position
-	if direction.length() > 25: 
-		mob.velocity = direction * move_speed * delta
-	else:
-		mob.velocity = Vector2()
+	if player:
+		navigation_agent_2d.target_position = player.global_position
+	if navigation_agent_2d.is_navigation_finished():
+		return
 	
-	if direction.length() > 70:
+	var current_agent_position = mob.global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	
+	var direction = player.global_position - mob.global_position
+	var new_velocity = current_agent_position.direction_to(next_path_position) * move_speed
+	
+	if navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity(new_velocity)
+	else:
+		_on_navigation_agent_2d_velocity_computed(new_velocity)
+		
+	if direction.length() > 160:
+		navigation_agent_2d.target_position = mob.global_position
 		state_transition.emit(self, "MobIdle")
 
 
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity):
+	mob.velocity = safe_velocity
+	
+	pass # Replace with function body.
--- Scripts/States/Mob/MobIdle.gd
+++ Scripts/States/Mob/MobIdle.gd
@@ -4,14 +4,17 @@ class_name MobIdle
 @onready var mob = $"../.."
 @export var move_speed := 20.0
 @onready var animated_sprite = $"../../AnimatedSprite2D"
+@onready var navigation_agent_2d = $"../../NavigationAgent2D"
 
 var player : CharacterBody2D
-var move_direction : Vector2
+var next_wander_point : Vector2
 var wander_time : float
 
 
 func randomize_wander():
-	move_direction = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
+	navigation_agent_2d.set_velocity(Vector2.ZERO)
+	wait(2.0)
+	next_wander_point = mob.global_position + Vector2(randf_range(-100,100), randf_range(-100,100))
 	wander_time = randf_range(1, 3)
 
 func enter():
@@ -25,12 +28,14 @@ func update(delta : float):
 	else: 
 		randomize_wander()
 
-func physics_update(delta: float):
+func physics_update(delta: float):	
 	if mob:
-		mob.velocity = move_direction * move_speed
+		var new_velocity = mob.global_position.direction_to(next_wander_point) * move_speed
+		navigation_agent_2d.set_velocity(new_velocity)
 	var direction = player.global_position - mob.global_position
 	
-	if direction.length() < 50:
+	if direction.length() < 100:
 		state_transition.emit(self, "MobFollow")
-	
-	
+
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
